a6a1c869fcd74f237e7dd5e3573cbe5a13f9400a
Xpression <emmybanks03@gmail.com>
2026-01-22 21:07:23 +0100

    finished design for hero section and the navbar

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,8 +1,47 @@
+import {Phone, User, Heart, ShoppingCart} from "lucide-react";
+import Image from "next/image";
 
 export default function Nav() {
-    
+  return (
+    <nav>
+      <div className="hidden sm:flex justify-evenly">
+        <ul className="flex gap-5 text-sm">
+          <li>Home</li>
+          <li>Category</li>
+          <li>Products</li>
+          <li>Pages</li>
+          <li>Blog</li>
+        </ul>
+        <div className="hidden sm:flex gap-2">
+          <Phone size={17} />
+          <p>+123 (456) (789)</p>
+        </div>
+      </div>
 
-    return (
-        
-    );
+      <div className="hidden sm:border border-gray-200" />
+
+      <div className="flex justify-between px-2 sm:justify-evenly items-center">
+
+      <div className="flex gap-1 items-center">
+        <Image src="/assets/chef.png" width={70} height={70} alt="app icon"/>
+        <p className="flex flex-col -gap-2"> <span className="font-extrabold text-sm">Foodzy</span> <span className="text-[11px]">A Treasure of Tastes</span></p>
+      </div>
+
+      <p className="sm:hidden">menu</p>
+
+      <div className="hidden border border-b-cyan-300 sm:flex w-80 justify-between items-center gap-1 ">
+        <input className="w-[59%]"/>
+        <p className="border-cyan-300 border-l p4 text-[11px]">All Categories</p>
+        <button className="p4 bg-black text-white text-[11px]">search</button>
+      </div>
+
+      <div className="hidden sm:flex items-center gap-3">
+        <p className="flex flex-col items-center text-[12px]"><User size={18}/> Account</p>
+        <p className="flex flex-col items-center text-[12px]"><Heart size={18}/> Wishlist</p>
+        <p className="flex flex-col items-center text-[12px]"><ShoppingCart size={18}/> Cart</p>
+      </div>
+
+      </div>
+    </nav>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,48 @@
+import Nav from "./components/nav";
+import { ArrowRightCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
-
-
 export default function Page() {
-  
-
   return (
-    
+    <>
+      <header>
+        <Nav />
+
+        <div className="hero bg-(image:--hero-background)">
+          <div className="max-w-7xl mx-auto px-6 py-20 sm:flex">
+            <div className="flex flex-col gap-1">
+              <p className="text-[15px] tracking-wide font-semibold text-gray-800">
+                Super Delicious
+              </p>
+              <h4 className="text-4xl md:text-5xl xl:text-6xl font-extrabold leading-tight text-white">
+                THE BEST WAY TO <br /> STUFF YOUR WALLET.
+              </h4>
+              <p className="max-w-md text-[12px] text-gray-800 font-semibold">
+                Today&apos;s Best Deal
+              </p>
+
+              <Button
+                variant="outline"
+                size="sm"
+                className="bg-order-button rounded-2xl max-w-30"
+              >
+                <ArrowRightCircle className="bg-white text-order-button rounded-full border-0 text-4xl" />{" "}
+                <span className="text-sm">ORDER NOW</span>
+              </Button>
+            </div>
+
+            <div className="relative flex justify-center md:justify-end">
+              <Image
+                src="/assets/spag.png"
+                width={500}
+                height={500}
+                alt="hero image"
+                className="max-w-full md:w-120 xl:w-150 object-contain drop-shadow-2xl"
+              />
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
-
