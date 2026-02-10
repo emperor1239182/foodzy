d24d336bbe97f54829f6d2daab11b188e1208c10
Xpression <emmybanks03@gmail.com>
2026-02-10 05:40:15 +0100

    feat: enhance Nav component with dropdown menus for categories and products; add search input group

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,28 +1,90 @@
 "use client";
-import { Phone, User, Heart, ShoppingCart, Menu, XIcon } from "lucide-react";
+import {
+  Phone,
+  User,
+  Heart,
+  ShoppingCart,
+  Menu,
+  XIcon,
+  Search,
+  ArrowDown,
+} from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
-import { Inter } from "next/font/google";
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import {
+  InputGroup,
+  InputGroupButton,
+  InputGroupInput,
+  InputGroupAddon,
+} from "@/components/ui/input-group";
 
-const inter = Inter({ 
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  display: 'swap',
-})
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
 
 export default function Nav() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
- 
+
   return (
     <nav>
       <div className="hidden sm:flex justify-evenly">
-        <ul className="flex gap-5 text-sm">
-          <li>Home</li>
-          <li>Category</li>
-          <li>Products</li>
-          <li>Pages</li>
-          <li>Blog</li>
+        <ul className="flex items center gap-5 text-sm">
+          <Button variant="link">Home</Button>
+          <li>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="link">
+                  Category <ArrowDown />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuGroup>
+                  <DropdownMenuLabel>All Categories</DropdownMenuLabel>
+                  <DropdownMenuItem>Main Dish</DropdownMenuItem>
+                  <DropdownMenuItem>Dessert</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Breakfast</DropdownMenuItem>
+                  <DropdownMenuItem>Breakfast Food</DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </li>
+
+          <li>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="link">
+                  Products <ArrowDown />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuGroup>
+                  <DropdownMenuLabel>Popular</DropdownMenuLabel>
+                  <DropdownMenuItem>Vegetables</DropdownMenuItem>
+                  <DropdownMenuItem>Dessert</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Fruits</DropdownMenuItem>
+                  <DropdownMenuItem>Snacks</DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </li>
+
+          <Button variant="link">Pages</Button>
+          <Button variant="link">Blog</Button>
         </ul>
         <div className="hidden sm:flex items-center gap-2">
           <Phone size={17} />
@@ -36,7 +98,6 @@ export default function Nav() {
         <div className="flex items-center ">
           <Image src="/assets/chef.png" width={70} height={70} alt="app icon" />
           <p className="flex flex-col ">
-            
             <span className="font-extrabold text-sm">Foodzy</span>
             <span className="text-[11px]">A Treasure of Tastes</span>
           </p>
@@ -44,34 +105,45 @@ export default function Nav() {
 
         <Menu className="sm:hidden" onClick={() => setIsOpen(true)} />
 
-    {isOpen && (
-  <nav className="w-full h-screen bg-accent z-20 fixed inset-0 text-2xl p-4 animate-in slide-in-from-right duration-800">
-    <XIcon className="absolute top-2 right-3" onClick={()=> setIsOpen(false)}/>
-    <ul className={cn("flex flex-col gap-8 items-center mt-9")}>
-      <li>Home</li>
-      <li>Category</li>
-      <li>Products</li>
-      <li>Pages</li>
-      <li>Blog</li>
-      <li className="flex flex-col items-center text-[12px]">
-        <User size={18} /> Account
-      </li>
-      <li className="flex flex-col items-center text-[12px]">
-        <Heart size={18} /> Wishlist
-      </li>
-      <li className="flex flex-col items-center text-[12px]">
-        <ShoppingCart size={18} /> Cart
-      </li>
-    </ul>
-  </nav>
-)}
+        {isOpen && (
+          <nav className="w-full h-screen bg-accent z-20 fixed inset-0 text-2xl p-4 animate-in slide-in-from-right duration-800">
+            <XIcon
+              className="absolute top-2 right-3"
+              onClick={() => setIsOpen(false)}
+            />
+            <ul className={cn("flex flex-col gap-8 items-center mt-9")}>
+              <li>Home</li>
+              <li>Category</li>
+              <li>Products</li>
+              <li>Pages</li>
+              <li>Blog</li>
+              <li className="flex flex-col items-center text-[12px]">
+                <User size={18} /> Account
+              </li>
+              <li className="flex flex-col items-center text-[12px]">
+                <Heart size={18} /> Wishlist
+              </li>
+              <li className="flex flex-col items-center text-[12px]">
+                <ShoppingCart size={18} /> Cart
+              </li>
+            </ul>
+          </nav>
+        )}
 
-        <div className="hidden border border-b-cyan-300 sm:flex w-80 justify-between items-center gap-1 ">
-          <input className="w-[59%]" />
-          <p className="border-cyan-300 border-l p4 text-[11px]">
-            All Categories
-          </p>
-          <button className="p4 bg-black text-white text-[11px]">search</button>
+        <div className="hidden sm:block lg:w-xl">
+          <InputGroup>
+            <InputGroupInput
+              placeholder="Search for items..."
+              id="inline-end-input"
+            />
+            <InputGroupAddon align="inline-end">
+              <Search size={10} />
+            </InputGroupAddon>
+            <InputGroupButton>
+              All Categories
+              <ArrowDown />
+            </InputGroupButton>
+          </InputGroup>
         </div>
 
         <div className="hidden sm:flex items-center gap-3">
